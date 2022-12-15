04adee9f0245558d856cdf7fa18e8f25ca8b814c
radheyshyam033 <107915565+radhey36@users.noreply.github.com>
2022-12-16 01:23:59 +0530

    adding icon

--- main.js
+++ main.js
@@ -16,3 +16,31 @@ function mob_next_btn() {
 function hamburger(){
     document.getElementById('nav-list').classList.toggle('side-menu')
 }
+function validateForm(){
+    let name=document.getElementById("name").value;
+    if(name==""){
+        text="Please enter your name";
+        document.getElementById('name-error').innerHTML=text
+        return false;
+    }
+    else{
+        text="";
+        document.getElementById('name-error').innerHTML=text
+        return true
+    }
+    let email=document.getElementById("email").value;
+    if(email==""){
+        text="Please enter your email";
+        document.getElementById('email-error').innerHTML=text
+        return false;
+    }
+    let password=document.getElementById("password").value;
+    if(password==""){
+        text="Please enter your password";
+        document.getElementById('password-error').innerHTML=text
+        return false;
+    }
+}
+function scrollTop(){
+    document.documentElement.scrollTop = 0;
+}
